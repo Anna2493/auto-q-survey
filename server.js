6c85103bcec4e8a8748e34327e79bafa1308fa90
Anna2493 <adawidowska24@gmail.com>
2021-01-15 20:54:09 +0000

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const Admins = require('./backend/routes/Admins')
