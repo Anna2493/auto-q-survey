212e749bfc360ec68946147d1373792f1535ebc8
Anna2493 <adawidowska24@gmail.com>
2021-01-15 20:33:46 +0000

    update

--- server.js
+++ server.js
@@ -3,14 +3,39 @@ const cors = require('cors') //Dont use cors on production
 const bodyParser = require('body-parser') //Handles JSON. Extracts
 // the data send from client side 
 const app = express() //instantiate express
-const port =  process.env.PORT || 5000 //3000
+const port = process.env.PORT || 5000 //3000
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://auto-q-survey-web.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const Admins = require('./backend/routes/Admins')
 app.use('/', Admins)
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, function() {
   console.log('Server is running on port: ' + port)
