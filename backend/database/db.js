6ab2a0f17ec8fc7186b56ff120ce11d350e66d73
Anna2493 <adawidowska24@gmail.com>
2021-01-15 22:19:10 +0000

    update

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,20 +1,7 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('auto_q_survey_db', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql', //type of database that is going to be used
-  operatorsAliases: false, //used to create complex comparision
-
-  pool: { //connection part with sequilize
-    max: 5, //maximum number of connections
-    min: 0, //minimum number of connections
-    acquire: 30000, //miliseconds time of making connection before making an error
-    idle: 10000 //milkisecond time for how long the connection can be idle
-  }
-})
-
-// const sequelize = new Sequelize('heroku_dc0786e4b1681e5', 'bc04907ee2af0b', '130f69b5', {
-//   host: 'eu-cdbr-west-03.cleardb.net',
+// const sequelize = new Sequelize('auto_q_survey_db', 'root', 'password', {
+//   host: 'localhost',
 //   dialect: 'mysql', //type of database that is going to be used
 //   operatorsAliases: false, //used to create complex comparision
 
@@ -25,6 +12,19 @@ const sequelize = new Sequelize('auto_q_survey_db', 'root', 'password', {
 //     idle: 10000 //milkisecond time for how long the connection can be idle
 //   }
 // })
+
+const sequelize = new Sequelize('heroku_dede944307935c0', 'b3d459f9c76d32', '6494ac05', {
+  host: 'eu-cdbr-west-03.cleardb.net',
+  dialect: 'mysql', //type of database that is going to be used
+  operatorsAliases: false, //used to create complex comparision
+
+  pool: { //connection part with sequilize
+    max: 5, //maximum number of connections
+    min: 0, //minimum number of connections
+    acquire: 30000, //miliseconds time of making connection before making an error
+    idle: 10000 //milkisecond time for how long the connection can be idle
+  }
+})
 
 
 
